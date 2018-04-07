ecfeaacfb3b1ad129c24a0a4b3cf711c55812ece
Linda Goldstein <lindag@squareup.com>
2018-04-06 21:58:33 -0700

    rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+# require database cleaner at the top level
+require 'database_cleaner'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configuration do |config|
+  config.include FactoryBot::Syntax::Methods
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
