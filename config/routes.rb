5bad78ef8cde1c8e8163b798924fe4a50521892e
Linda Goldstein <lindag@squareup.com>
2018-04-15 21:00:32 -0700

    default route is get all campaigns

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :campaign_hosts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'campaign_hosts#index'
+
+  resources :campaign_hosts
 end
